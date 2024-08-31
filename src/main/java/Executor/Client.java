45424db0b6150fb23119e53b19d7aa6d59b0ca7d
DHIVAKER29 <ssdddhivaker@gmail.com>
2024-08-31 20:03:36 +0530

    Executors framework learning

---
+++ src/main/java/Executor/Client.java
@@ -0,0 +1,16 @@
+
+package Executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client {
+    public static void main(String[] args) {
+        ExecutorService ex = Executors.newCachedThreadPool();
+        //ExecutorService ex2 = Executors.newCachedThreadPool();
+        for(int i = 1 ; i <= 10 ; i++){
+            PrintNumber t = new PrintNumber(i);
+            ex.submit(t);
+        }
+    }
+}
